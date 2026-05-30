33c32ca2a373fad55d103e4580e9d4578d3d144e
all-in-team <164168089+all-in-team@users.noreply.github.com>
2026-05-31 02:48:20 +0800

    Update apply_patch.py

--- apply_patch.py
+++ apply_patch.py
@@ -41,4 +41,60 @@ def _notify_admin_of_pending(c_user):
 '''
 
 marker = "# module variables"
-if marker n
+if marker not in content:
+    print(f"ERROR: marker '{marker}' not found")
+    sys.exit(1)
+content = content.replace(marker, helper + marker, 1)
+
+old_block = '''	# create new user
+	user = User()
+	user.defaults()
+	user.id = c_user.id
+	updateUserFromEvent(user, c_user)
+	if not any(db.iterateUserIds()):
+		user.rank = RANKS.admin
+
+	logging.info("%s joined chat", user)
+	db.addUser(user)
+	ret = [rp.Reply(rp.types.CHAT_JOIN)]
+
+	motd = db.getSystemConfig().motd
+	if motd:
+		ret.append(rp.Reply(rp.types.CUSTOM, text=motd))
+
+	return ret'''
+
+new_block = '''	# create new user
+	user = User()
+	user.defaults()
+	user.id = c_user.id
+	updateUserFromEvent(user, c_user)
+
+	is_first = not any(db.iterateUserIds())
+	if is_first:
+		user.rank = RANKS.admin
+		logging.info("%s joined chat as first user (admin)", user)
+		db.addUser(user)
+		ret = [rp.Reply(rp.types.CHAT_JOIN)]
+		motd = db.getSystemConfig().motd
+		if motd:
+			ret.append(rp.Reply(rp.types.CUSTOM, text=motd))
+		return ret
+
+	user.rank = RANKS.banned
+	user.blacklistReason = "PENDING_APPROVAL"
+	logging.info("%s joined as PENDING (awaiting approval)", user)
+	db.addUser(user)
+	_notify_admin_of_pending(c_user)
+	return [rp.Reply(rp.types.CUSTOM, text="\u23f3 Your join request has been sent to the admin. You will be notified once approved.")]'''
+
+if old_block not in content:
+    print("ERROR: old_block not found in core.py")
+    sys.exit(1)
+
+content = content.replace(old_block, new_block)
+
+with open(CORE_PATH, "w") as f:
+    f.write(content)
+
+print("Pending patch applied successfully")
